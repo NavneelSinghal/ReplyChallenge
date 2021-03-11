ee33bc16124ca311186da07d4766ee993bb97002
Navneel Singhal <navneel.singhal@ymail.com>
2021-03-12 01:14:36 +0530

    first commit

---
+++ old.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2")
+#pragma GCC optimization("O3")
+#pragma GCC optimization("unroll-loops")
+using namespace std;
+
+#define F first
+#define S second
+
+typedef long long ll;
+using pii = pair<long long int, long long int>;
+
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
+//#include <ext/rope>
+
+using namespace __gnu_pbds;
+// using namespace __gnu_cxx;
+
+using ll = long long;
+
+// #define int long long
+#define unordered_map gp_hash_table
+#define cauto const auto
+
+template <typename T>
+void ignore_unused(const T &) {}
+#ifdef DEBUG
+#define debug(args...)                               \
+    {                                                \
+        std::string _s = #args;                      \
+        replace(_s.begin(), _s.end(), ',', ' ');     \
+        std::stringstream _ss(_s);                   \
+        std::istream_iterator<std::string> _it(_ss); \
+        err(_it, args);                              \
+    }
+#define print_container(v)          \
+    {                               \
+        bool first = true;          \
+        os << "[";                  \
+        for (auto x : v) {          \
+            if (!first) os << ", "; \
+            os << x;                \
+            first = false;          \
+        }                           \
+        return os << "]";           \
+    }
+void err(std::istream_iterator<std::string> it) { ignore_unused(it); }
+template <typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+    std::cerr << *it << " = " << a << std::endl;
+    err(++it, args...);
+}
+
+// clang-format off
+
+template <typename T1, typename T2> inline std::ostream& operator << (std::ostream& os, const std::pair<T1, T2>& p) { return os << "(" << p.first << ", " << p.second << ")"; }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::vector<T>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::set<T>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::map<T1, T2>& v) { print_container(v); }
+template <typename T1, typename T2, class C> inline std::ostream &operator << (std::ostream & os, const unordered_map<T1, T2, C>& v) { print_container(v); }
+template <typename T, class C> inline std::ostream &operator << (std::ostream & os, const unordered_set<T, C>& v) { print_container(v); }
+template <typename T1, typename T2> inline std::ostream &operator << (std::ostream & os, const std::multimap<T1, T2>& v) { print_container(v); }
+template <typename T> inline std::ostream &operator << (std::ostream & os, const std::multiset<T>& v) { print_container(v); }
+#else
+#define debug(args...) 0
+#endif
+
+template <typename X, typename Y> X &remin(X &x, const Y &y) { return x = (y < x) ? y : x; }
+template <typename X, typename Y> X &remax(X &x, const Y &y) { return x = (x < y) ? y : x; }
+template <typename X, typename Y> [[nodiscard]] bool ckmin(X &x, const Y &y) { return (y < x) ? (x = y, 1) : 0; }
+template <typename X, typename Y> [[nodiscard]] bool ckmax(X &x, const Y &y) { return (x < y) ? (x = y, 1) : 0; }
+template <typename T> inline T sq(T a) { return a * a; }
+template <typename T> inline T sq_dist(pair<T, T> &a, pair<T, T> &b) { return sq(a.first - b.first) + sq(a.second - b.second); }
+template <typename T> inline long double dist(pair<T, T> &a, pair<T, T> &b) { return sqrtl(sq_dist(a, b)); }
+inline long long isqrt(long long n) {
+    long long sq = (long long)sqrtl((long double)n) - 2;
+    sq = max(sq, 0LL);
+    while (sq * sq < n) sq++;
+    if ((sq * sq) == n) return sq;
+    return sq - 1;
+}
+inline bool is_sq(int n) { int w = (int)isqrt(n); return w * w == n; }
+
+struct custom_hash {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
+struct pair_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+    size_t operator()(pair<int, int> p) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(p.first * 31 + p.second + FIXED_RANDOM);
+    }
+};
+
+void setIO(string name = "") {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << setprecision(10) << fixed;
+    if (name.size() == 0) return;
+    FILE *fin = freopen((name + ".in").c_str(), "r", stdin);
+    FILE *fout = freopen((name + ".out").c_str(), "w", stdout);
+    ignore_unused(fin);
+    ignore_unused(fout);
+}
+
+// clang-format on
+
+// Please write the recurances once :(
+void pre() {}
+ll w, h;
+ll n, m, r;
+// grid
+//
+struct Build {
+    ll bx, by, bl, bc;
+    Build() {}
+    Build(int a) { cin >> bx >> by >> bl >> bc; }
+};
+struct Antan {
+    ll ar, ac;
+    Antan() {}
+    Antan(int a) { cin >> ar >> ac; }
+};
+
+void solve() {
+    cin >> w >> h;
+    cin >> n >> m >> r;
+
+    vector<Build> v(n);
+    vector<Antan> a(m);
+
+    for (int i = 0; i < n; i++) {
+        v[i] = Build(0);
+    }
+
+    for (int i = 0; i < m; i++) {
+        a[i] = Antan(0);
+    }
+
+    vector<pair<Antan, int>> an(m);
+    for (int i = 0; i < m; ++i) {
+        an[i] = make_pair(a[i], i);
+    }
+    
+    sort(begin(an), end(an), [&] (const pair<Antan, int>& p1, const pair<Antan, int>& p2) {
+            return p1.first.ac > p2.first.ac;
+        });
+
+    sort(begin(v), end(v), [&] (const Build& b1, const Build& b2) {
+            return b1.bc > b2.bc;
+        });
+
+
+    const int sz = min(n, m);
+    cout << sz << '\n';
+    for (int i = 0; i < sz; ++i) {
+        cout << an[i].second << ' ' << v[i].bx << ' ' << v[i].by << '\n';
+    }
+
+    cout.flush();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    pre();
+    long long int num = 1;
+    string files[] = {"a", "b", "c", "d", "e", "f"};
+    for (int TT = 0; TT < 6; TT++) {
+        string inp = files[TT] + ".in";
+        string out = files[TT] + ".out";
+        freopen(inp.c_str(), "r", stdin);
+        freopen(out.c_str(), "w", stdout);
+        solve();
+    }
+}
+
